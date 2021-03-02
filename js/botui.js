1932581ea931f7f6bf8498fc91525fab23fa3308
InitiativeYu.github.io <1361947230@qq.com>
2021-03-02 16:25:35 +0800

    Site updated: 2021-03-02 16:25:33

--- js/botui.js
+++ js/botui.js
@@ -26,7 +26,7 @@ function bot_ui_ini() {
     }).then(function () {
         botui.message.add({
             delay: 1100,
-            content: "这里是InitiativeYu"
+            content: "这里是 Mashiro"
         }).then(function () {
             botui.message.add({
                 delay: 1100,
@@ -65,11 +65,11 @@ function bot_ui_ini() {
         secondpart = function () {
             botui.message.add({
                 delay: 1500,
-                content: "目前普通本科在校"
+                content: "目前就读于上海财经大学"
             }).then(function () {
                 botui.message.add({
                     delay: 1500,
-                    content: "喜欢技术，向往自由，阳光温柔..."
+                    content: "向往技术却误入商科，但后来喜欢上了经济学…"
                 }).then(function () {
                     botui.message.add({
                         delay: 1200,
@@ -77,22 +77,21 @@ function bot_ui_ini() {
                     }).then(function () {
                         botui.message.add({
                             delay: 1500,
-                            content: "主攻 Java方向~
- HTML/CSS/JavaScript/PHP"
+                            content: "主攻 R 语言和 Python，略懂 STATA，偶尔也折腾 HTML/CSS/JavaScript/PHP"
                         }).then(function () {
                             botui.message.add({
                                 delay: 1500,
-                                content: "略略略"
+                                content: "研究的方向，是经济/金融方向的数据分析（data science）以及机器学习（machine learning）"
                             }).then(function () {
                                 botui.message.add({
                                     delay: 1800,
-                                    content: "不知该说什么了..."
+                                    content: "喜欢画画，希望有一天能够被称为画师"
                                 }).then(function () {
                                     botui.action.button({
                                         delay: 1100,
                                         action: [{
-                                            text: "为什么叫InitiativeYu呢？ 🤔",
-                                            value: "why-InitiativeYu"
+                                            text: "为什么叫Mashiro呢？ 🤔",
+                                            value: "why-mashiro"
                                         }]
                                     }).then(function (a) {
                                         thirdpart()
@@ -107,7 +106,7 @@ function bot_ui_ini() {
         thirdpart = function () {
             botui.message.add({
                 delay: 1E3,
-                content: "Initiative有主动的，有创造性的意思，Yu是我名字的最后一个字~"
+                content: "Mashiro以及站名都来自一部动画，因为和主角有一样的爱好~ 如果有兴趣可以找找首页上的视频~"
             }).then(function () {
                 botui.action.button({
                     delay: 1500,
@@ -127,12 +126,12 @@ function bot_ui_ini() {
             }).then(function () {
                 botui.message.add({
                     delay: 1100,
-                    content: "而且我挺喜欢猫的，我女朋友也超爱~hahah"
+                    content: "而且我真的是猫控！"
                 }).then(function () {
                     botui.action.button({
                         delay: 1500,
                         action: [{
-                            text: "域名有什么含义吗？(ง •_•)ง",
+                            text: "域名有什么含意吗？(ง •_•)ง",
                             value: "why-domain"
                         }]
                     }).then(function (a) {
@@ -144,11 +143,11 @@ function bot_ui_ini() {
         fifthpart = function () {
             botui.message.add({
                 delay: 1E3,
-                content: "yulab言简意赅吧..."
+                content: "emmmm，看备案信息你就知道了=.= 本来想要zheng.xin的，但50万真买不起。。"
             }).then(function () {
                 botui.message.add({
                     delay: 1600,
-                    content: "那么，仔细看看我的博客吧？ ^_^祝你每天都有一个好心情"
+                    content: "那么，仔细看看我的博客吧？ ^_^"
                 })
             })
         } 
